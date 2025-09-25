c6dc172e6208118989d99cf3fe7e96d3b7159481
zuhaird17 <zuhaird17@gmail.com>
2025-09-26 03:49:09 +0600

    New Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="bg-slate-200 w-full h-[100px] flex items-center justify-center">
       <h1 className="text-6xl font-bold text-center text-white space-x-1 opacity-20">
-        {['M', 'Y', ' ', 'C', 'O', 'L', 'O', 'R', ' ', 'R', 'E', 'A', 'C', 'T', ' ', 'A', 'P', 'P'].map((letter, index) => (
+        {['C', 'H', ' ', 'O', 'O', 'S', 'E', 'M', 'Y', 'P', 'A', 'L', 'L', 'E', 'T'].map((letter, index) => (
           <span
             key={index}
             className={`inline-block animate-rainbow text-${['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'][index % 7]}-500 text-shadow-xl`}
