e35fc32db314d4a240ae158eaf7a34fe6ee990f3
zuhaird17 <zuhaird17@gmail.com>
2025-09-27 23:50:41 +0600

    Modification & cache cleaning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,16 @@ export function Header() {
   return (
     <header className="bg-slate-200 w-full h-[100px] flex items-center justify-center">
       <h1 className="text-6xl font-bold text-center text-white space-x-1 opacity-20">
-        {['M','a','c','P','a','l','l','e','t'].map((letter, index) => (
-          <span
-            key={index}
-            className={`inline-block animate-rainbow text-${['white'][index % 7]}-500 text-shadow-xl`}
-            style={{ animationDelay: `${index * 0.1}s` }} // Staggered animation for each letter
-          >
-            {letter}
-          </span>
-        ))}
+        {['M','a','c',' ',' ',' ','P','a','l','l','e','t'].map((letter, index) => (
+  <span
+    key={index}
+    className={`inline-block animate-rainbow ${letter === ' ' ? 'mx-2' : 'text-white text-shadow-xl'}`}
+    style={{ animationDelay: `${index * 0.1}s` }}
+  >
+    {letter}
+  </span>
+))}
+
       </h1>
     </header>
   );
